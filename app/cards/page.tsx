fe6c02a71d6989922b863a826df71b5218299b95
murari <muraric@gmail.com>
2025-09-22 17:22:52 -0400

    Updated security

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic"; // ⛔ prevent static export
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic"; // ⛔ prevent static export
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
