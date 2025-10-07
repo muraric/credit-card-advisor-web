ce8bff8e4dd6ef27e282f9a233408a00db56d57f
Murari <muraric@gmail.com>
2025-10-07 00:10:45 -0400

    android app development

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
+    reactStrictMode: true,
+    swcMinify: true,
 
+    // 👇 This tells Next.js to generate static HTML in the /out folder
+    output: 'export',
+};
 module.exports = nextConfig;
